dac9c04712c585e4e34b1895c4f7bd8580f04e5f
Noxius <96249252+Noxius18@users.noreply.github.com>
2023-10-12 10:21:29 +0700

    Create SK1.py

---
+++ Pemdas/Pertemuan_1/SK1.py
@@ -0,0 +1,6 @@
+print("================================")
+print("Nama\t: Mohammad Adam Nur")
+print("NIM\t: 19230359")
+print("Kelas\t: 19.1A.39")
+print("Jurusan\t: Sistem Informasi")
+print("================================")
